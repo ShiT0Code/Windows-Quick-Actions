82d9921d47f8a45f154ab4f30096283bde3ca471
ShiT0Code <hbeauwor@outlook.com>
2026-03-08 11:04:01 +0800

    侧边栏新增“全部”页面及导航按钮
    为侧边栏主页面添加“全部”按钮，点击后可导航至新建的 AllItemsPage 页面，并实现返回功能。调整了主页面布局，将“全部操作”文本替换为按钮，并在项目文件中配置了新页面的编译项。

---
+++ WinodwsApp/TouchHelper/SideBar/AllItemsPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace TouchHelper.SideBar;
+public sealed partial class AllItemsPage : Page
+{
+    public AllItemsPage() => InitializeComponent();
+
+    private void Back_Button_Click(object sender, RoutedEventArgs e) => this.Frame.GoBack();
+}
--- WinodwsApp/TouchHelper/SideBar/SideBarMainPage.xaml.cs
+++ WinodwsApp/TouchHelper/SideBar/SideBarMainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
 
 namespace TouchHelper.SideBar;
 public sealed partial class SideBarMainPage : Page
 {
-    public SideBarMainPage()
-    {
-        InitializeComponent();
-    }
+    public SideBarMainPage() => InitializeComponent();
+
+    private void AllItems_Button_Click(object sender, RoutedEventArgs e)
+        => this.Frame.Navigate(typeof(AllItemsPage), null, new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight });
 }
